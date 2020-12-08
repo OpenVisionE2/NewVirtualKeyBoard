2d3af058c6d4ec077057074dffac7fdd8206cab8
RAED <rrrr53@hotmail.com>
2020-12-08 16:05:22 +0300

    Add files via upload

--- usr/lib/enigma2/python/Plugins/SystemPlugins/NewVirtualKeyBoard/plugin.py
+++ usr/lib/enigma2/python/Plugins/SystemPlugins/NewVirtualKeyBoard/plugin.py
@@ -1,19 +1,41 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+
 from Plugins.Plugin import PluginDescriptor
+from Components.config import config
+from enigma import getDesktop
+from Plugins.SystemPlugins.NewVirtualKeyBoard.VirtualKeyBoard import *
+
+screenwidth = getDesktop(0).size()
 
 def main(session, **kwargs):
-    from .VirtualKeyBoard import nvKeyboardSetup
-    session.open(nvKeyboardSetup)
+	from .VirtualKeyBoard import nvKeyboardSetup
+	session.open(nvKeyboardSetup)
+
 
 def menu(menuid, **kwargs):
-    if menuid == 'system':
-        return [(_('VirtualKeyBoard setup'),
-          main,
-          'virtulkeyBoard_setup',
-          None)]
-    else:
-        return []
+	if menuid == 'system':
+		return [(_('VirtualKeyBoard setup'), main, 'virtulkeyBoard_setup', None)]
+	else:
+		return []
+
+ICONFILE = 'images/plugin-icon_sd.png'
+if screenwidth.width() > 1280:
+	ICONFILE = 'images/plugin-icon.png'
+DES = (_('Setup virtual keyboard'))
+PNAME = (_('VirtualKeyboard'))
+pluginlist = PluginDescriptor(name=PNAME, description=DES, where=PluginDescriptor.WHERE_PLUGINMENU, icon=ICONFILE, fnc=main, needsRestart=False)
 
 def Plugins(**kwargs):
-    return [PluginDescriptor(name=_('VirtualKeyBoard'), description=_('setup virtual keyboard'), where=PluginDescriptor.WHERE_MENU, needsRestart=False, fnc=menu)]
+	result = [
+		PluginDescriptor(
+			name=PNAME,
+			description = DES,
+			where = PluginDescriptor.WHERE_MENU,
+			fnc = menu,
+			needsRestart=False
+		),
+	]
+	if config.NewVirtualKeyBoard.showinplugins.value:
+		result.append(pluginlist)
+	return result
