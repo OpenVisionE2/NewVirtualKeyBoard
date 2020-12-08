bd8f1e0686c9b70a3816a6c594da73580cb7bdcd
RAED <rrrr53@hotmail.com>
2020-12-08 16:04:30 +0300

    Add files via upload

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 pkg = 'SystemPlugins.NewVirtualKeyBoard'
 setup (name = 'enigma2-plugin-systemplugins-newvirtualkeyboard',
-       version = '1.0',
+       version = '1.1',
        description = 'New VirtualKeyBoard Style.',
        packages = [pkg],
        package_dir = {pkg: 'usr'},
