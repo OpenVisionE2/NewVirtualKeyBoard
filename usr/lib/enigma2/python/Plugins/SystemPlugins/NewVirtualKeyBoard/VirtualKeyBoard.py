ea9a53ec3f044b930ee7758960c86bd731846e9d
persianpros <persianpros@yahoo.com>
2022-09-06 17:51:29 +0430

    Follow new xml variables in py files as well, see https://github.com/OpenVisionE2/enigma2-openvision/commit/0648087d9c54fd94f669d624d56d17df9e9c90ff

--- usr/lib/enigma2/python/Plugins/SystemPlugins/NewVirtualKeyBoard/VirtualKeyBoard.py
+++ usr/lib/enigma2/python/Plugins/SystemPlugins/NewVirtualKeyBoard/VirtualKeyBoard.py
@@ -1659,28 +1659,28 @@ class nvKeyboardSetup(ConfigListScreen, Screen):
         	<screen name="nvKeyboardSetup" position="center,center" size="1080,300" backgroundColor="#16000000" title="New Virtual Keyboard Settings  V %s">
             	<widget name="config" position="30,30" size="1020,190" itemHeight="45" font="Regular;30" scrollbarMode="showOnDemand" transparent="1" zPosition="2" />
 
-            	<ePixmap position="30,230" size="38,38" pixmap="~/images/key_red.png" zPosition="3" transparent="1" alphatest="blend" />
-             	<eLabel position="78,230" zPosition="4" size="300,38" valign="center" font="Regular;30" transparent="1" foregroundColor="#ffffff" backgroundColor="#41000000" text="Cancel" />
+            	<ePixmap position="30,230" size="38,38" pixmap="~/images/key_red.png" zPosition="3" transparent="1" alphaTest="blend" />
+             	<eLabel position="78,230" zPosition="4" size="300,38" verticalAlignment="center" font="Regular;30" transparent="1" foregroundColor="#ffffff" backgroundColor="#41000000" text="Cancel" />
 
-            	<ePixmap position="330,230" size="38,38" pixmap="~/images/key_green.png" zPosition="3" transparent="1" alphatest="blend" />
-            	<eLabel position="378,230" zPosition="4" size="300,38" valign="center" font="Regular;30" transparent="1" foregroundColor="#ffffff" backgroundColor="#41000000" text="Save" />
+            	<ePixmap position="330,230" size="38,38" pixmap="~/images/key_green.png" zPosition="3" transparent="1" alphaTest="blend" />
+            	<eLabel position="378,230" zPosition="4" size="300,38" verticalAlignment="center" font="Regular;30" transparent="1" foregroundColor="#ffffff" backgroundColor="#41000000" text="Save" />
 
-            	<ePixmap position="630,230" size="38,38" pixmap="~/images/key_yellow.png" zPosition="3" transparent="1" alphatest="blend" />
-            	<eLabel position="678,230" zPosition="4" size="420,38" valign="center" font="Regular;30" transparent="1" foregroundColor="#ffffff" backgroundColor="#41000000" text="Virtual keyboard" />
+            	<ePixmap position="630,230" size="38,38" pixmap="~/images/key_yellow.png" zPosition="3" transparent="1" alphaTest="blend" />
+            	<eLabel position="678,230" zPosition="4" size="420,38" verticalAlignment="center" font="Regular;30" transparent="1" foregroundColor="#ffffff" backgroundColor="#41000000" text="Virtual keyboard" />
         	</screen>""" % VER
     else:
         skin = """
         	<screen name="nvKeyboardSetup" position="center,center" size="720,190" backgroundColor="#16000000" title="New Virtual Keyboard Settings  V %s">
             	<widget name="config" position="20,20" size="680,345" itemHeight="30" font="Regular;20" scrollbarMode="showOnDemand" transparent="1" zPosition="2" />
 
-            	<ePixmap position="20,150" size="25,25" pixmap="~/images/key_red_sd.png" zPosition="3" transparent="1" alphatest="blend" />
-            	<eLabel position="52,150" zPosition="4" size="200,25" valign="center" font="Regular;20" transparent="1" foregroundColor="#ffffff" backgroundColor="#41000000" text="Cancel" />
+            	<ePixmap position="20,150" size="25,25" pixmap="~/images/key_red_sd.png" zPosition="3" transparent="1" alphaTest="blend" />
+            	<eLabel position="52,150" zPosition="4" size="200,25" verticalAlignment="center" font="Regular;20" transparent="1" foregroundColor="#ffffff" backgroundColor="#41000000" text="Cancel" />
 
-            	<ePixmap position="220,150" size="25,25" pixmap="~/images/key_green_sd.png" zPosition="3" transparent="1" alphatest="blend" />
-            	<eLabel position="252,150" zPosition="4" size="200,25" valign="center" font="Regular;20" transparent="1" foregroundColor="#ffffff" backgroundColor="#41000000" text="Save" />
+            	<ePixmap position="220,150" size="25,25" pixmap="~/images/key_green_sd.png" zPosition="3" transparent="1" alphaTest="blend" />
+            	<eLabel position="252,150" zPosition="4" size="200,25" verticalAlignment="center" font="Regular;20" transparent="1" foregroundColor="#ffffff" backgroundColor="#41000000" text="Save" />
 
-            	<ePixmap position="420,150" size="25,25" pixmap="~/images/key_yellow_sd.png" zPosition="3" transparent="1" alphatest="blend" />
-            	<eLabel position="452,150" zPosition="4" size="280,25" valign="center" font="Regular;20" transparent="1" foregroundColor="#ffffff" backgroundColor="#41000000" text="Virtual keyboard" />
+            	<ePixmap position="420,150" size="25,25" pixmap="~/images/key_yellow_sd.png" zPosition="3" transparent="1" alphaTest="blend" />
+            	<eLabel position="452,150" zPosition="4" size="280,25" verticalAlignment="center" font="Regular;20" transparent="1" foregroundColor="#ffffff" backgroundColor="#41000000" text="Virtual keyboard" />
         	</screen>""" % VER
 
     def __init__(self, session, fromkeyboard=False):
